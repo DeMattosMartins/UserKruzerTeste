b11a5127dd047aa0f86aa74f4cc654ee9eb16830
MateusDeMattosMartins <mateus.martins@kruzer.io>
2025-11-03 00:01:10 -0300

    feat: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { createGetUsersFactory } from './features/Users/actions/getUsers/getUsersFactory';
 import { createDeleteUserFactory } from './features/Users/actions/deleteUser/deleteUserFactory';
 import { createUserFactory } from './features/Users/actions/createUser/createUserFactory';
 import { createGetUserFactory } from './features/Users/actions/getUser/getUserFactory';
 import { createUpdateUserFactory } from './features/Users/actions/updateUser/updateUserFactory';
 
+dotenv.config();
+
 const app = express();
 app.use(express.json());
 
-const MONGO_URI = 'mongodb+srv://demattosmartins_db_user:JywuX43MX6V1jPsb@cluster0.etuirep.mongodb.net/UserCollection';
+const MONGO_URI = process.env.MONGO_URI;
 
 async function conectarMongo() {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(String(MONGO_URI));
     console.log('Conectado ao MongoDB');
   } catch (err) {
     console.error('Erro ao conectar ao MongoDB', err);
